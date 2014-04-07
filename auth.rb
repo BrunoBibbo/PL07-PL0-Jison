4f7999a2e2eecfad46652915a18b51e77f2e21f7
juanma3108 <alu0100597929@ull.edu.es>
2014-04-07 23:06:45 +0100

    cambios en auth.rb

--- auth.rb
+++ auth.rb
@@ -1,5 +1,8 @@
 require 'omniauth-oauth2'
 require 'omniauth-google-oauth2'
+require 'sinatra'
+require "sinatra/reloader" if development?
+require 'sinatra/flash'
 
 use OmniAuth::Builder do
   config = YAML.load_file 'config/config_template.yml'
