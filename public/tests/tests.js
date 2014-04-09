07e4787cbb67396fc8ac4625dc086408c858bd00
unknown <alu0100697063@ull.edu.es>
2014-04-10 00:40:16 +0100

    Pruebas finalizadas.

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -62,7 +62,7 @@ var assert = chai.assert;
  
   test('Varios statements (Begin - End): ', function(){  
     var input = pl0.parse("IF (b >= a) THEN a = b ELSE BEGIN CALL proc(); a = 3; b = 4; END.");
-    assert.equal('[{"Type":"BLOCK","CONTENT":{"CONSTS":{"Type":"CONST","Constants":[[{"Type":"=","left":{"ID":"a"},"right":{"Value":"50"}},{"Type":"=","left":{"ID":"b"},"right":{"Value":"200"}}]]},"VARS":{"Type":"VAR","Variables":[[{"Variable":"x"},{"Variable":"y"}]]},"STATEMENTS":{"Type":"=","left":{"ID":"z"},"right":{"Value":{"Type":"NUMBER","Value":"10"}}}}}]', JSON.stringify(input));
+    assert.equal('[{"Type":"BLOCK","CONTENT":{"CONSTS":[],"VARS":[],"PROCEDURE":[],"STATEMENTS":{"Type":"IFELSE","left":{"Condition":{"Type":">=","left":{"term":{"Type":"ID","Value":"b"}},"right":{"term":{"Type":"ID","Value":"a"}}}},"center":{"Statement":{"Type":"=","left":{"ID":"a"},"right":{"Value":{"Type":"ID","Value":"b"}}}},"right":{"Statement":{"Type":"BEGINEND","Expressions":{"Statement":[{"Type":"CALL","Procedure":{"ID":"proc","Arguments":[]}},{"Type":"=","left":{"ID":"a"},"right":{"Value":{"Type":"NUMBER","Value":"3"}}},{"Type":"=","left":{"ID":"b"},"right":{"Value":{"Type":"NUMBER","Value":"4"}}}]}}}}}}]', JSON.stringify(input));
   });
   
  });
@@ -71,22 +71,22 @@ var assert = chai.assert;
  
   test('Const: ', function(){  
     var input = pl0.parse("CONST a = 6, b = 7; IF (a < b) THEN a = a+1.");
-    assert.equal('[{"Type":"BLOCK","CONTENT":{"PROCEDURE":[{"Type":"PROCEDURE","ID":"a","Arguments":[],"Block":{"Type":"BLOCK","CONTENT":{"VARS":{"Type":"VAR","Variables":[[{"Variable":"a"},{"Variable":"v"}]]},"STATEMENTS":{"Type":"=","left":{"ID":"a"},"right":{"Value":{"Type":"NUMBER","Value":"3"}}}}}},null],"STATEMENTS":{"Type":"=","left":{"ID":"v"},"right":{"Value":{"Type":"NUMBER","Value":"4"}}}}}]', JSON.stringify(input));
+    assert.equal('[{"Type":"BLOCK","CONTENT":{"CONSTS":{"Type":"CONST","Constants":[[{"Type":"=","left":{"ID":"a"},"right":{"Value":"6"}},{"Type":"=","left":{"ID":"b"},"right":{"Value":"7"}}]]},"VARS":[],"PROCEDURE":[],"STATEMENTS":{"Type":"IF","left":{"Condition":{"Type":"<","left":{"term":{"Type":"ID","Value":"a"}},"right":{"term":{"Type":"ID","Value":"b"}}}},"right":{"Statement":{"Type":"=","left":{"ID":"a"},"right":{"Value":{"Type":"+","left":{"term":{"Type":"ID","Value":"a"}},"right":{"term":{"Type":"NUMBER","Value":"1"}}}}}}}}}]', JSON.stringify(input));
   });
   
   test('Var: ', function(){  
     var input = pl0.parse("VAR a, b; BEGIN a = 5; b = 7; IF (a < b) THEN a = a+1; END.");
-    assert.equal('[{"Type":"BLOCK","CONTENT":{"PROCEDURE":[{"Type":"PROCEDURE","ID":"a","Arguments":[],"Block":{"Type":"BLOCK","CONTENT":{"VARS":{"Type":"VAR","Variables":[[{"Variable":"a"},{"Variable":"v"}]]},"STATEMENTS":{"Type":"=","left":{"ID":"a"},"right":{"Value":{"Type":"NUMBER","Value":"3"}}}}}},null],"STATEMENTS":{"Type":"=","left":{"ID":"v"},"right":{"Value":{"Type":"NUMBER","Value":"4"}}}}}]', JSON.stringify(input));
+    assert.equal('[{"Type":"BLOCK","CONTENT":{"CONSTS":[],"VARS":{"Type":"VAR","Variables":[[{"Variable":"a"},{"Variable":"b"}]]},"PROCEDURE":[],"STATEMENTS":{"Type":"BEGINEND","Expressions":{"Statement":[{"Type":"=","left":{"ID":"a"},"right":{"Value":{"Type":"NUMBER","Value":"5"}}},{"Type":"=","left":{"ID":"b"},"right":{"Value":{"Type":"NUMBER","Value":"7"}}},{"Type":"IF","left":{"Condition":{"Type":"<","left":{"term":{"Type":"ID","Value":"a"}},"right":{"term":{"Type":"ID","Value":"b"}}}},"right":{"Statement":{"Type":"=","left":{"ID":"a"},"right":{"Value":{"Type":"+","left":{"term":{"Type":"ID","Value":"a"}},"right":{"term":{"Type":"NUMBER","Value":"1"}}}}}}}]}}}}]', JSON.stringify(input));
   });
   
   test('Procedure - Sin argumentos: ', function(){  
     var input = pl0.parse("PROCEDURE a(); b = 5; CALL a().");
-    assert.equal('[{"Type":"BLOCK","CONTENT":{"PROCEDURE":[{"Type":"PROCEDURE","ID":"a","Arguments":[],"Block":{"Type":"BLOCK","CONTENT":{"VARS":{"Type":"VAR","Variables":[[{"Variable":"a"},{"Variable":"v"}]]},"STATEMENTS":{"Type":"=","left":{"ID":"a"},"right":{"Value":{"Type":"NUMBER","Value":"3"}}}}}},null],"STATEMENTS":{"Type":"=","left":{"ID":"v"},"right":{"Value":{"Type":"NUMBER","Value":"4"}}}}}]', JSON.stringify(input));
+    assert.equal('[{"Type":"BLOCK","CONTENT":{"CONSTS":[],"VARS":[],"PROCEDURE":[{"Type":"PROCEDURE","ID":"a","Arguments":[],"Block":{"Type":"BLOCK","CONTENT":{"CONSTS":[],"VARS":[],"PROCEDURE":[],"STATEMENTS":{"Type":"=","left":{"ID":"b"},"right":{"Value":{"Type":"NUMBER","Value":"5"}}}}}}],"STATEMENTS":{"Type":"CALL","Procedure":{"ID":"a","Arguments":[]}}}}]', JSON.stringify(input));
   });
   
   test('Procedure - Con argumentos: ', function(){  
     var input = pl0.parse("PROCEDURE a(c, d, e); b = 5; CALL a(c, d, e).");
-    assert.equal('[{"Type":"BLOCK","CONTENT":{"PROCEDURE":[{"Type":"PROCEDURE","ID":"a","Arguments":[],"Block":{"Type":"BLOCK","CONTENT":{"VARS":{"Type":"VAR","Variables":[[{"Variable":"a"},{"Variable":"v"}]]},"STATEMENTS":{"Type":"=","left":{"ID":"a"},"right":{"Value":{"Type":"NUMBER","Value":"3"}}}}}},null],"STATEMENTS":{"Type":"=","left":{"ID":"v"},"right":{"Value":{"Type":"NUMBER","Value":"4"}}}}}]', JSON.stringify(input));
+    assert.equal('[{"Type":"BLOCK","CONTENT":{"CONSTS":[],"VARS":[],"PROCEDURE":[{"Type":"PROCEDURE","ID":"a","Arguments":[{"Type":"ID","Value":"c"},{"Type":"ID","Value":"d"},{"Type":"ID","Value":"e"}],"Block":{"Type":"BLOCK","CONTENT":{"CONSTS":[],"VARS":[],"PROCEDURE":[],"STATEMENTS":{"Type":"=","left":{"ID":"b"},"right":{"Value":{"Type":"NUMBER","Value":"5"}}}}}}],"STATEMENTS":{"Type":"CALL","Procedure":{"ID":"a","Arguments":[{"Type":"ID","Value":"c"},{"Type":"ID","Value":"d"},{"Type":"ID","Value":"e"}]}}}}]', JSON.stringify(input));
   });
   
  });
@@ -95,7 +95,7 @@ var assert = chai.assert;
  
   test('Programa de prueba: ', function(){  
     var input = pl0.parse("CONST a = 0, b = 3; VAR c, d, e; PROCEDURE alfa(a, b); z = 3; BEGIN WHILE (a == b) DO IF (c < e) THEN d = c+a ELSE CALL alfa(a, b); END.");
-    assert.equal('[{"Type":"BLOCK","CONTENT":{"CONSTS":{"Type":"CONST","Constants":[[{"Type":"=","left":{"ID":"a"},"right":{"Value":"50"}},{"Type":"=","left":{"ID":"b"},"right":{"Value":"200"}}]]},"VARS":{"Type":"VAR","Variables":[[{"Variable":"x"},{"Variable":"y"}]]},"STATEMENTS":{"Type":"=","left":{"ID":"z"},"right":{"Value":{"Type":"NUMBER","Value":"10"}}}}}]', JSON.stringify(input));
+    assert.equal('[{"Type":"BLOCK","CONTENT":{"CONSTS":{"Type":"CONST","Constants":[[{"Type":"=","left":{"ID":"a"},"right":{"Value":"0"}},{"Type":"=","left":{"ID":"b"},"right":{"Value":"3"}}]]},"VARS":{"Type":"VAR","Variables":[[{"Variable":"c"},{"Variable":"d"},{"Variable":"e"}]]},"PROCEDURE":[{"Type":"PROCEDURE","ID":"alfa","Arguments":[{"Type":"ID","Value":"a"},{"Type":"ID","Value":"b"}],"Block":{"Type":"BLOCK","CONTENT":{"CONSTS":[],"VARS":[],"PROCEDURE":[],"STATEMENTS":{"Type":"=","left":{"ID":"z"},"right":{"Value":{"Type":"NUMBER","Value":"3"}}}}}}],"STATEMENTS":{"Type":"BEGINEND","Expressions":{"Statement":[{"Type":"WHILEDO","left":{"Condition":{"Type":"==","left":{"term":{"Type":"ID","Value":"a"}},"right":{"term":{"Type":"ID","Value":"b"}}}},"right":{"Statement":{"Type":"IFELSE","left":{"Condition":{"Type":"<","left":{"term":{"Type":"ID","Value":"c"}},"right":{"term":{"Type":"ID","Value":"e"}}}},"center":{"Statement":{"Type":"=","left":{"ID":"d"},"right":{"Value":{"Type":"+","left":{"term":{"Type":"ID","Value":"c"}},"right":{"term":{"Type":"ID","Value":"a"}}}}}},"right":{"Statement":{"Type":"CALL","Procedure":{"ID":"alfa","Arguments":[{"Type":"ID","Value":"a"},{"Type":"ID","Value":"b"}]}}}}}}]}}}}]', JSON.stringify(input));
   });
 
  });
